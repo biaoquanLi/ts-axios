f8728d08708e21dae351d0eb76c4dbcfcd0bb7ee
李标泉 <1083756620@qq.com>
2021-01-05 00:11:09 +0800

    完成接口扩展功能

--- examples/server.js
+++ examples/server.js
@@ -5,7 +5,6 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const router = express.Router()
 const webpackConfig = require('./webpack.config')
-const { extend } = require('../src/helpers/util')
 
 const app = express()
 const compiler = webpack(webpackConfig)
@@ -21,7 +20,7 @@ app.use(webpackDevMiddleware(compiler, {
 simple()
 base()
 error()
-extend()
+registerExtend()
 
 
 
@@ -38,7 +37,7 @@ module.exports = app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
 
-function simple(){
+function simple() {
     router.get('/simple/get', (req, res) => {
         res.json({
             msg: 'hello world'
@@ -46,16 +45,16 @@ function simple(){
     })
 }
 
-function base(){
+function base() {
     router.get('/base/get', function (req, res) {
         res.json(req.query)
     })
-    
+
     router.post('/base/post', function (req, res) {
         console.log(111, req.body)
         res.json(req.body)
     })
-    
+
     router.post('/base/buffer', function (req, res) {
         let msg = []
         req.on('data', (chunk) => {
@@ -69,7 +68,7 @@ function base(){
         })
     })
 }
-function error(){
+function error() {
     router.get('/error/get', function (req, res) {
         if (Math.random() > 0.5) {
             res.json({
@@ -80,7 +79,7 @@ function error(){
             res.end()
         }
     })
-    
+
     router.get('/error/timeout', function (req, res) {
         setTimeout(() => {
             res.json({
@@ -89,8 +88,38 @@ function error(){
         }, 3000)
     })
 }
-function extend(){
-    router.post('/extend/post',(req,res)=>{
+function registerExtend() {
+    router.get('/extend/get', (req, res) => {
+        res.json({
+            msg: 'hello world'
+        })
+    })
+    router.post('/extend/post', (req, res) => {
         res.json(req.body)
+    })
+    router.options('/extend/options', (req, res) => {
+        res.end()
+    })
+    router.delete('/extend/delete', (req, res) => {
+        res.end()
+    })
+    router.head('/extend/head', (req, res) => {
+        res.end()
+    })
+    router.options('/extend/options', (req, res) => {
+        res.end()
+    })
+    router.put('/extend/put', (req, res) => {
+        res.json(req.body)
+    })
+    router.patch('/extend/patch', (req, res) => {
+        res.json(req.body)
+    })
+    router.get('/extend/user', (req, res) => {
+        res.json({
+            code: 200,
+            result: { name: '小明', age: 30 },
+            message: '获取成功'
+        })
     })
 }
